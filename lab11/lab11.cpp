ed5f33b0c77978a65e4879b1c4f741076095ea18
Maddie Bird <birdma@butte.edu>
2017-09-04 22:32:05 +0000

    Lab1.2 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,31 @@
+1. Program asks user to guess a number between 1 and 100.
+2. User makes first guess.
+3. If the user guesses correctly, they get 10 points and the program starts over.
+4. If the user did not guess correctly they lose one point and the program indicates whether the correct number is higher or lower than the user's first guess.
+5. User makes second guess based on program's indications.
+6. If the user guesses correctly, they get 9 points and the program starts over.
+7. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's second guess.
+8. User makes third guess based on program's prior indications.
+9. If the user guesses correctly, they get 8 points and the program starts over.
+10. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's third guess.
+11. User makes fourth guess based on program's prior indications.
+12. If the user guesses correctly, they get 7 points and the program starts over.
+13. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's fourth guess.
+14. User makes fifth guess based on program's prior indications.
+15. If the user guesses correctly, they get 6 points and the program starts over.
+16. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's fifth guess.
+17. User makes sixth guess based on program's prior indications.
+18. If the user guesses correctly, they get 5 points and the program starts over.
+19. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's sixth guess.
+20. User makes seventh guess based on program's prior indications.
+21. If the user guesses correctly, they get 4 points and the program starts over.
+22. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's seventh guess.
+23. User makes eighth guess based on program's prior indications.
+24. If the user guesses correctly, they get 3 points and the program starts over.
+25. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's eighth guess.
+26. User makes ninth guess based on program's prior indications.
+27. If the user guesses correctly, they get 2 points and the program starts over.
+28. If the user did not guess correctly they lose one more point and the program indicates whether the correct number is higher or lower than the user's ninth guess.
+29. User makes tenth guess based on program's prior indications.
+30. If the user guesses correctly, they get 1 points and the program starts over.
+31. If the user did not guess correctly, they get 0 points, the program reveals the correct number and starts over.
---
+++ lab12/lab12.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     cout<<"     --&--    "<<endl;
+     cout<<"   /       \\  "<<endl;
+     cout<<"  /  _   _  \\ "<<endl;
+     cout<<"C|   o   o   |D"<<endl;
+     cout<<"  \\    `    / "<<endl;
+     cout<<"   \\  (O)  / "<<endl;
+     cout<<"     ----- "<<endl;
+     
+ }
